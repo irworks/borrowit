37bf5328aa0777db89a5a65fc680b0adc5ef499d
Ilja Rozhko <ilja@oktobyte.com>
2023-07-30 14:58:33 +0200

    + Added general translation strings

---
+++ lang/de/general.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | General Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for general utility words and
+    | phrases throughout the application.
+    |
+    */
+
+    'yes' => 'Ja',
+    'no' => 'Nein',
+    'edit' => 'Bearbeiten',
+    'show-all' => 'Alle anzeigen',
+
+];
---
+++ lang/en/general.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | General Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used for general utility words and
+    | phrases throughout the application.
+    |
+    */
+
+    'yes' => 'Yes',
+    'no' => 'No',
+    'edit' => 'Edit',
+    'show-all' => 'Show all',
+
+];
