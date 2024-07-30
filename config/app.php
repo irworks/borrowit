6d8858714992af5231994eba7df8dac1facfe3a1
Ilja Rozhko <ilja@oktobyte.com>
2024-07-30 09:54:34 +0200

    ~ Fix routing by not using asset() in config files

--- config/app.php
+++ config/app.php
@@ -17,7 +17,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'Laravel'),
-    'logo' => env('APP_LOGO', asset('images/logo/borrow-it-logo.svg')),
+    'logo' => env('APP_LOGO', '/images/logo/borrow-it-logo.svg'),
     'version' => [
         'branch' => env('BUILD_VERSION_BRANCH', 'unknown'),
         'commit' => env('BUILD_VERSION_COMMIT', 'unknown')
